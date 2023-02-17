96f28ea1f9f9c16a5bdd7c0c0088eeb4bfcbc571
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-17 17:41:33 -0500

    Created object for office file

---
+++ public/logs/offices.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+require_once 'MySQLDatabase.php';
+require_once 'Office.php';
+require_once 'Response.php';
+
+$database = new MySQLDatabase();
+$office = new Office($database);
+$output = $office->findAll();
+$response = new Response();
+$response->toJson($output);
