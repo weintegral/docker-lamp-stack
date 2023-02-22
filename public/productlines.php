88376abc2f0cb9c03711083c0d1de553647d93ce
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-21 19:09:57 -0500

    Updated productline File

--- public/productlines.php
+++ public/productlines.php
@@ -1,34 +1,43 @@
 <?php
 declare(strict_types = 1);
+require_once 'Request.php';
 require_once 'MySQLDatabase.php';
 require_once 'ProductLine.php';
 require_once 'Response.php';
+require_once 'Debug.php';
 
+$requestObj = new Request();
 $response = new Response();
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+if ($requestObj->getRequestType() === 'GET') {
     try {
-        $userProvidedProductLineId = (string)$_GET['productLineId'];
+        $urlPath= $requestObj->getRequestPath();
+        $urlPathData = explode( '/', $urlPath);
         $database = new MySQLDatabase();
-        $productLine = new ProductLine($database);
-        $output = $productLine->findById($userProvidedProductLineId);
+        $productline = new ProductLine($database);
+        if (!isset($urlPathData[2]))
+        {
+            $output = $productline->findAll();
+            $response->toJson($output);
+        }
+        $userProvidedProductLineId =(string)$urlPathData[2];
+        $output = $productline->findById($userProvidedProductLineId);
         $response->toJson($output);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'Database issue']);
-    }
-    catch (LogicException $exception) {
-        $response->toJson(['status' => 'logic issue']);
+    } catch (InvalidArgumentException $exception) {
+        $response->toJson(['status' => $exception->getMessage()]);
     }
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($requestObj->getRequestType() === 'POST') {
     try {
-        $json = file_get_contents('php://input');
-        $userProvidedData = (array)json_decode($json);
+        $userProvidedData = $requestObj->getRequestBody();
         $database = new MySQLDatabase();
-        $productLine = new ProductLine($database);
-        $productLine->insert($userProvidedData);
-        $response->toJson(['status' => 'success']);
+        $productline = new ProductLine($database);
+        $productline->insert($userProvidedData);
+        $response->setResponseCode(200)
+            ->toJson(['status' => 'success']);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'failure']);
     }
