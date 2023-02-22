be9a0dc9e30f546c766ab6fbe97b0ccb95af5ff8
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 12:45:37 -0500

    commit products object

--- public/products.php
+++ public/products.php
@@ -1,14 +1,44 @@
 <?php
 declare(strict_types = 1);
+require_once 'Request.php';
 require_once 'MySQLDatabase.php';
 require_once 'Product.php';
 require_once 'Response.php';
+require_once 'Debug.php';
 
-$database = new MySQLDatabase();
-$product = new Product($database);
-$output = $product->findAll();
+$requestObj = new Request();
 $response = new Response();
-$response->toJson($output);
 
+if ($requestObj->getRequestType() === 'GET') {
+    try {
+        $urlPath= $requestObj->getRequestPath();
+        $urlPathData = explode( '/', $urlPath);
+        $database = new MySQLDatabase();
+        $product = new Product($database);
+        if (!isset($urlPathData[2]))
+        {
+            $output = $product->findAll();
+            $response->toJson($output);
+        }
+        $userProvidedProductId =(string)$urlPathData[2];
+        $output = $product->findById($userProvidedProductId);
+        $response->toJson($output);
+    } catch (PDOException $exception) {
+        $response->toJson(['status' => 'Database issue']);
+    } catch (InvalidArgumentException $exception) {
+        $response->toJson(['status' => $exception->getMessage()]);
+    }
+}
 
-
+if ($requestObj->getRequestType() === 'POST') {
+    try {
+        $userProvidedData = $requestObj->getRequestBody();
+        $database = new MySQLDatabase();
+        $product = new Product($database);
+        $product->insert($userProvidedData);
+        $response->setResponseCode(200)
+            ->toJson(['status' => 'success']);
+    } catch (PDOException $exception) {
+        $response->toJson(['status' => 'failure']);
+    }
+}
