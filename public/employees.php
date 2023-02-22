0785493f633f7e3f6aa8feffab0e56addb50de97
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-21 19:11:10 -0500

    Updated employees File

--- public/employees.php
+++ public/employees.php
@@ -1,34 +1,43 @@
 <?php
 declare(strict_types = 1);
+require_once 'Request.php';
 require_once 'MySQLDatabase.php';
 require_once 'Employee.php';
 require_once 'Response.php';
+require_once 'Debug.php';
 
+$requestObj = new Request();
 $response = new Response();
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+if ($requestObj->getRequestType() === 'GET') {
     try {
-        $userProvidedEmployerId = (int)$_GET['employeeId'];
+        $urlPath= $requestObj->getRequestPath();
+        $urlPathData = explode( '/', $urlPath);
         $database = new MySQLDatabase();
         $employee = new Employee($database);
-        $output = $employee->findById($userProvidedEmployerId);
+        if (!isset($urlPathData[2]))
+        {
+            $output = $employee->findAll();
+            $response->toJson($output);
+        }
+        $userProvidedCustomerId =(int)$urlPathData[2];
+        $output = $employee->findById($userProvidedCustomerId);
         $response->toJson($output);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'Database issue']);
-    }
-    catch (LogicException $exception) {
-        $response->toJson(['status' => 'logic issue']);
+    } catch (InvalidArgumentException $exception) {
+        $response->toJson(['status' => $exception->getMessage()]);
     }
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($requestObj->getRequestType() === 'POST') {
     try {
-        $json = file_get_contents('php://input');
-        $userProvidedData = (array)json_decode($json);
+        $userProvidedData = $requestObj->getRequestBody();
         $database = new MySQLDatabase();
         $employee = new Employee($database);
         $employee->insert($userProvidedData);
-        $response->toJson(['status' => 'success']);
+        $response->setResponseCode(200)
+            ->toJson(['status' => 'success']);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'failure']);
     }
