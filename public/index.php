19343811506986ae978087d337bfdd2b81816c90
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-21 19:11:27 -0500

    Updated Index File

--- public/index.php
+++ public/index.php
@@ -1,63 +1,86 @@
 <?php
 declare(strict_types = 1);
-require 'MySQLDatabase.php';
-require 'Customer.php';
-require 'Response.php';
+require_once 'Request.php';
 
 /**
- * C.R.U.D
- * C: Create (POST)
- * R: Read (GET)
- * U: Update (PUT, PATCH)
- * D: Delete (DELETE)
- */
+* C.R.U.D
+* C: Create (POST)
+    * R: Read (GET)
+* U: Update (PUT, PATCH)
+* D: Delete (DELETE)
+*/
 
 /**
- * TOPICS that are covered
- *
- * PHP built in Web server
- * defining variables
- * SUPER GLOBAL Variables
- * data types in PHP
- * PDO Object
- * PDO Options
- * Error Handling
- * Debugging and Printing in PHP
- * loops in PHP
- * Namespaces
- * Request and Response Headers
- * HTTP Methods
- * HTTP Status Codes
- */
-$response = new Response();
+* TOPICS that are covered
+*
+* PHP built in Web server
+* defining variables
+* SUPER GLOBAL Variables
+* data types in PHP
+* PDO Object
+* PDO Options
+* Error Handling
+* Debugging and Printing in PHP
+* loops in PHP
+* Namespaces
+* Request and Response Headers
+* HTTP Methods
+* HTTP Status Codes
+*/
 
+$requestObj = new Request();
+$isCustomerRequest = str_contains($requestObj->getRequestPath(), 'customers');
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    try {
-        $userProvidedCustomerId = (int)$_GET['customerId'];
-        $database = new MySQLDatabase();
-        $customer = new Customer($database);
-        $output = $customer->findById($userProvidedCustomerId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    }
-    catch (LogicException $exception) {
-      $response->toJson(['status' => 'logic issue']);
-    }
+if($isCustomerRequest) {
+require_once 'customers.php';
 }
 
+$requestObj = new Request();
+$isEmployeeRequest = str_contains($requestObj->getRequestPath(), 'employees');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $json = file_get_contents('php://input');
-        $userProvidedData = (array)json_decode($json);
-        $database = new MySQLDatabase();
-        $customer = new Customer($database);
-        $customer->insert($userProvidedData);
-        $response->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+if($isEmployeeRequest) {
+    require_once 'employees.php';
+}
+
+$requestObj = new Request();
+$isOrderRequest = str_contains($requestObj->getRequestPath(), 'orders');
+
+if($isOrderRequest) {
+    require_once 'orders.php';
+}
+
+$requestObj = new Request();
+$isOrderDetailRequest = str_contains($requestObj->getRequestPath(), 'orderdetails');
+
+if($isOrderDetailRequest) {
+    require_once 'orderdetails.php';
+}
+
+$requestObj = new Request();
+$isOfficeRequest = str_contains($requestObj->getRequestPath(), 'offices');
+
+if($isOfficeRequest) {
+    require_once 'offices.php';
+}
+
+$requestObj = new Request();
+$isPaymentRequest = str_contains($requestObj->getRequestPath(), 'payments');
+
+if($isPaymentRequest) {
+    require_once 'payments.php';
+}
+
+$requestObj = new Request();
+$isProductRequest = str_contains($requestObj->getRequestPath(), 'products');
+
+if($isProductRequest) {
+    require_once 'products.php';
+}
+
+$requestObj = new Request();
+$isProductLinesRequest = str_contains($requestObj->getRequestPath(), 'productlines');
+
+if($isProductLinesRequest) {
+    require_once 'productlines.php';
 }
 
