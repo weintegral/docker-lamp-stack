f36368ab7befd63f1d7079116ef67a214c62c9b8
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-21 19:12:15 -0500

    Updated orders File

--- public/orders.php
+++ public/orders.php
@@ -1,34 +1,43 @@
 <?php
 declare(strict_types = 1);
+require_once 'Request.php';
 require_once 'MySQLDatabase.php';
 require_once 'Order.php';
 require_once 'Response.php';
+require_once 'Debug.php';
 
+$requestObj = new Request();
 $response = new Response();
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+if ($requestObj->getRequestType() === 'GET') {
     try {
-        $userProvidedOrderId = (int)$_GET['orderId'];
+        $urlPath= $requestObj->getRequestPath();
+        $urlPathData = explode( '/', $urlPath);
         $database = new MySQLDatabase();
         $order = new Order($database);
+        if (!isset($urlPathData[2]))
+        {
+            $output = $order->findAll();
+            $response->toJson($output);
+        }
+        $userProvidedOrderId =(int)$urlPathData[2];
         $output = $order->findById($userProvidedOrderId);
         $response->toJson($output);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'Database issue']);
-    }
-    catch (LogicException $exception) {
-        $response->toJson(['status' => 'logic issue']);
+    } catch (InvalidArgumentException $exception) {
+        $response->toJson(['status' => $exception->getMessage()]);
     }
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($requestObj->getRequestType() === 'POST') {
     try {
-        $json = file_get_contents('php://input');
-        $userProvidedData = (array)json_decode($json);
+        $userProvidedData = $requestObj->getRequestBody();
         $database = new MySQLDatabase();
         $order = new Order($database);
         $order->insert($userProvidedData);
-        $response->toJson(['status' => 'success']);
+        $response->setResponseCode(200)
+            ->toJson(['status' => 'success']);
     } catch (PDOException $exception) {
         $response->toJson(['status' => 'failure']);
     }
