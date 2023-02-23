1302a7a0ce508cca09e69f8edbe97da8d35ce83c
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-22 19:37:57 -0500

    Updated offices file

--- public/offices.php
+++ public/offices.php
@@ -1,44 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'Request.php';
-require_once 'MySQLDatabase.php';
-require_once 'Office.php';
-require_once 'Response.php';
-require_once 'Debug.php';
 
-$requestObj = new Request();
-$response = new Response();
+$officeControllerObj = ObjectContainer:: officeController();
+$requestObj = ObjectContainer::request();
 
 if ($requestObj->getRequestType() === 'GET') {
-    try {
-        $urlPath= $requestObj->getRequestPath();
-        $urlPathData = explode( '/', $urlPath);
-        $database = new MySQLDatabase();
-        $office = new Office($database);
-        if (!isset($urlPathData[2]))
-        {
-            $output = $office->findAll();
-            $response->toJson($output);
-        }
-        $userProvidedOfficeId =(int)$urlPathData[2];
-        $output = $office->findById($userProvidedOfficeId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    } catch (InvalidArgumentException $exception) {
-        $response->toJson(['status' => $exception->getMessage()]);
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $officeControllerObj->listAction();
+        die;
     }
+    echo $officeControllerObj->getByIdAction();
+    die;
 }
 
 if ($requestObj->getRequestType() === 'POST') {
-    try {
-        $userProvidedData = $requestObj->getRequestBody();
-        $database = new MySQLDatabase();
-        $office = new Office($database);
-        $office->insert($userProvidedData);
-        $response->setResponseCode(200)
-            ->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+    echo $officeControllerObj->createAction();
+    die;
 }
