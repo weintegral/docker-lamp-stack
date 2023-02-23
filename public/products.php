5e144ce967dc0d528ccf6d09650a4ab9a29af7f9
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 23:52:19 -0500

    modified products object

--- public/products.php
+++ public/products.php
@@ -1,44 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'Request.php';
-require_once 'MySQLDatabase.php';
-require_once 'Product.php';
-require_once 'Response.php';
-require_once 'Debug.php';
 
-$requestObj = new Request();
-$response = new Response();
+$productControllerObj = ObjectContainer::productController();
+$requestObj = ObjectContainer::request();
 
 if ($requestObj->getRequestType() === 'GET') {
-    try {
-        $urlPath= $requestObj->getRequestPath();
-        $urlPathData = explode( '/', $urlPath);
-        $database = new MySQLDatabase();
-        $product = new Product($database);
-        if (!isset($urlPathData[2]))
-        {
-            $output = $product->findAll();
-            $response->toJson($output);
-        }
-        $userProvidedProductId =(string)$urlPathData[2];
-        $output = $product->findById($userProvidedProductId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    } catch (InvalidArgumentException $exception) {
-        $response->toJson(['status' => $exception->getMessage()]);
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $productControllerObj->listAction();
+        die;
     }
+    echo $productControllerObj->getByIdAction();
+    die;
 }
 
 if ($requestObj->getRequestType() === 'POST') {
-    try {
-        $userProvidedData = $requestObj->getRequestBody();
-        $database = new MySQLDatabase();
-        $product = new Product($database);
-        $product->insert($userProvidedData);
-        $response->setResponseCode(200)
-            ->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+    echo $productControllerObj->createAction();
+    die;
 }
