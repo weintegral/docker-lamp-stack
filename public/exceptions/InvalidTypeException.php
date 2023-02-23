43c8a3c412627a79f84d33543864328044f6e67a
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 23:55:48 -0500

    created invalid type exception in exception dir

---
+++ public/exceptions/InvalidTypeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Exceptions;
+
+class InvalidTypeException extends \Exception
+{
+
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
