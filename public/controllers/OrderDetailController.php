cc99c0a127049d7101a90eb634c83e66cdf55192
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-22 19:26:35 -0500

    Created OrderDetails Controller

---
+++ public/controllers/OrderDetailController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+class OrderDetailController
+{
+    private Response $responseObj;
+    private OrderDetailModel $orderDetailModel;
+
+    public function __construct()
+    {
+        $this->responseObj = ObjectContainer::response();
+        $this->orderDetailModel = ObjectContainer::orderDetailModel();
+    }
+
+    public function listAction(): string
+    {
+        try {
+            $output = $this->orderDetailModel->findAll();
+            return $this->responseObj->toJson($output);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        } catch (InvalidArgumentException) {
+            return $this->responseObj->toJson(['status' => 'Invalid Argument Exception']);
+        }
+    }
+
+    public function getByIdAction(): string
+    {
+        try {
+            $urlPath = ObjectContainer::request()->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedOrderDetailId = (int)$urlPathData[2];
+
+            $output = $this->orderDetailModel->findById($userProvidedOrderDetailId);
+            return $this->responseObj->toJson($output);
+        } catch (PDOException) {
+            return $this->responseObj->toJson(['status' => 'Database issue']);
+        } catch (LogicException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function createAction(): string
+    {
+        try {
+            $userProvidedData = ObjectContainer::request()->getRequestBody();
+            $this->orderDetailModel->insert($userProvidedData);
+            return $this->responseObj->setResponseCode(201)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+}
