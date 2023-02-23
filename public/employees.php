e3f0b10d229d91eb1a8a93951c9a97b8a371dfe1
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-22 19:28:06 -0500

    Updated employees.php

--- public/employees.php
+++ public/employees.php
@@ -1,44 +1,24 @@
 <?php
 declare(strict_types = 1);
-require_once 'Request.php';
-require_once 'MySQLDatabase.php';
-require_once 'Employee.php';
-require_once 'Response.php';
-require_once 'Debug.php';
 
-$requestObj = new Request();
-$response = new Response();
+$employeeControllerObj = ObjectContainer::employeeController();
+$requestObj = ObjectContainer::request();
 
 if ($requestObj->getRequestType() === 'GET') {
-    try {
-        $urlPath= $requestObj->getRequestPath();
-        $urlPathData = explode( '/', $urlPath);
-        $database = new MySQLDatabase();
-        $employee = new Employee($database);
-        if (!isset($urlPathData[2]))
-        {
-            $output = $employee->findAll();
-            $response->toJson($output);
-        }
-        $userProvidedCustomerId =(int)$urlPathData[2];
-        $output = $employee->findById($userProvidedCustomerId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    } catch (InvalidArgumentException $exception) {
-        $response->toJson(['status' => $exception->getMessage()]);
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $employeeControllerObj->listAction();
+
+        die;
     }
+    echo $employeeControllerObj->getByIdAction();
+
+    die;
 }
 
 if ($requestObj->getRequestType() === 'POST') {
-    try {
-        $userProvidedData = $requestObj->getRequestBody();
-        $database = new MySQLDatabase();
-        $employee = new Employee($database);
-        $employee->insert($userProvidedData);
-        $response->setResponseCode(200)
-            ->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+    echo $employeeControllerObj->createAction();
+
+    die;
 }
