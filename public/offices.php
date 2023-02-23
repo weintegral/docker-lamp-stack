4e1c7b16b4a2a01c53d02ff7f829111d010de798
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 23:50:10 -0500

    modified offices object

--- public/offices.php
+++ public/offices.php
@@ -1,11 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'MySQLDatabase.php';
-require_once 'Office.php';
-require_once 'Response.php';
 
-$database = new MySQLDatabase();
-$office = new Office($database);
-$output = $office->findAll();
-$response = new Response();
-$response->toJson($output);
+$officeControllerObj = ObjectContainer::officeController();
+$requestObj = ObjectContainer::request();
+
+if ($requestObj->getRequestType() === 'GET') {
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $officeControllerObj->listAction();
+        die;
+    }
+    echo $officeControllerObj->getByIdAction();
+    die;
+}
+
+if ($requestObj->getRequestType() === 'POST') {
+    echo $officeControllerObj->createAction();
+    die;
+}
