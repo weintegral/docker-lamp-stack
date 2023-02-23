a84e54d1c5f0e52596fb62dc994550e16ee5551a
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-22 19:38:24 -0500

    Deleted

--- public/CustomException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CustomException extends  Exception
-{
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
