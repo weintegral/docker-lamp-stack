ab9bee041950cea68891d39cedfe4d7e281b09e5
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-22 19:28:38 -0500

    Updated orderdetails.php

--- public/orderdetails.php
+++ public/orderdetails.php
@@ -1,44 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'Request.php';
-require_once 'MySQLDatabase.php';
-require_once 'OrderDetail.php';
-require_once 'Response.php';
-require_once 'Debug.php';
 
-$requestObj = new Request();
-$response = new Response();
+$orderdetailControllerObj = ObjectContainer::orderDetailController();
+$requestObj = ObjectContainer::request();
 
 if ($requestObj->getRequestType() === 'GET') {
-    try {
-        $urlPath= $requestObj->getRequestPath();
-        $urlPathData = explode( '/', $urlPath);
-        $database = new MySQLDatabase();
-        $orderDetails = new OrderDetail($database);
-        if (!isset($urlPathData[2]))
-        {
-            $output = $orderDetails->findAll();
-            $response->toJson($output);
-        }
-        $userProvidedOrderDetailId =(int)$urlPathData[2];
-        $output = $orderDetails->findById($userProvidedOrderDetailId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    } catch (InvalidArgumentException $exception) {
-        $response->toJson(['status' => $exception->getMessage()]);
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $orderdetailControllerObj->listAction();
+        die;
     }
+    echo $orderdetailControllerObj->getByIdAction();
+    die;
 }
 
 if ($requestObj->getRequestType() === 'POST') {
-    try {
-        $userProvidedData = $requestObj->getRequestBody();
-        $database = new MySQLDatabase();
-        $orderDetails = new OrderDetail($database);
-        $orderDetails->insert($userProvidedData);
-        $response->setResponseCode(200)
-            ->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+    echo $orderdetailControllerObj->createAction();
+    die;
 }
