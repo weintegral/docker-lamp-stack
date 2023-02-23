61a8c19b016a48405999a5bd18b68e6921e09488
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 23:50:59 -0500

    modified orders object

--- public/orders.php
+++ public/orders.php
@@ -1,12 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'MySQLDatabase.php';
-require_once 'Order.php';
-require_once 'Response.php';
 
-$database = new MySQLDatabase();
-$order = new Order($database);
-$output = $order->findAll();
-$response = new Response();
-$response->toJson($output);
+$orderControllerObj = ObjectContainer::orderController();
+$requestObj = ObjectContainer::request();
 
+if ($requestObj->getRequestType() === 'GET') {
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $orderControllerObj->listAction();
+        die;
+    }
+    echo $orderControllerObj->getByIdAction();
+    die;
+}
+
+if ($requestObj->getRequestType() === 'POST') {
+    echo $orderControllerObj->createAction();
+    die;
+}
