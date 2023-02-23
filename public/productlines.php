03f9fc9435cea86c76ba4d0c76c34d75de81c425
Kamal Siddharth Teki <sidteki84@gmail.com>
2023-02-22 23:51:56 -0500

    modified productlines object

--- public/productlines.php
+++ public/productlines.php
@@ -1,44 +1,21 @@
 <?php
 declare(strict_types = 1);
-require_once 'Request.php';
-require_once 'MySQLDatabase.php';
-require_once 'ProductLine.php';
-require_once 'Response.php';
-require_once 'Debug.php';
 
-$requestObj = new Request();
-$response = new Response();
+$productLineControllerObj = ObjectContainer::productLineController();
+$requestObj = ObjectContainer::request();
 
 if ($requestObj->getRequestType() === 'GET') {
-    try {
-        $urlPath= $requestObj->getRequestPath();
-        $urlPathData = explode( '/', $urlPath);
-        $database = new MySQLDatabase();
-        $productline = new ProductLine($database);
-        if (!isset($urlPathData[2]))
-        {
-            $output = $productline->findAll();
-            $response->toJson($output);
-        }
-        $userProvidedProductLineId =(string)$urlPathData[2];
-        $output = $productline->findById($userProvidedProductLineId);
-        $response->toJson($output);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'Database issue']);
-    } catch (InvalidArgumentException $exception) {
-        $response->toJson(['status' => $exception->getMessage()]);
+    $urlPathData = explode( '/', $requestObj->getRequestPath());
+    if (!isset($urlPathData[2]))
+    {
+        echo $productLineControllerObj->listAction();
+        die;
     }
+    echo $productLineControllerObj->getByIdAction();
+    die;
 }
 
 if ($requestObj->getRequestType() === 'POST') {
-    try {
-        $userProvidedData = $requestObj->getRequestBody();
-        $database = new MySQLDatabase();
-        $productline = new ProductLine($database);
-        $productline->insert($userProvidedData);
-        $response->setResponseCode(200)
-            ->toJson(['status' => 'success']);
-    } catch (PDOException $exception) {
-        $response->toJson(['status' => 'failure']);
-    }
+    echo $productLineControllerObj->createAction();
+    die;
 }
