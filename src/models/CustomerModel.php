927f9dacfe990028f2753d5839089bc5d54523aa
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-27 01:04:38 -0500

    Updated CustomerModel file

---
+++ src/models/CustomerModel.php
@@ -0,0 +1,160 @@
+<?php
+declare(strict_types=1);
+
+namespace App\models;
+
+use App\utils\MySQLDatabase;
+use App\utils\ObjectContainer;
+use LogicException;
+use PDOException;
+class CustomerModel
+{
+    private MySQLDatabase $database;
+    public function __construct()
+    {
+        $this->database = ObjectContainer::mysqlDB();
+    }
+
+    public function findAll(): array
+    {
+        $pdo = $this->database->getConnection();
+        $stmt = $pdo->query('SELECT * FROM customers');
+        $records = $stmt->fetchAll();
+        $output['customers'] = [];
+        foreach($records as $record) {
+            $customer = [];
+            $customer['name'] = $record['customerName'];
+            $customer['number']= $record['customerNumber'];
+            $customer['firstName']= $record['contactFirstName'];
+            $customer['lastName']= $record['contactLastName'];
+            $customer['phone'] = $record['phone'];
+            $customer['addressLine1']= $record['addressLine1'];
+            $customer['addressLine2']= $record['addressLine2'];
+            $customer['city']= $record['city'];
+            $customer['state']= $record['state'];
+            $customer['postalCode']= $record['postalCode'];
+            $customer['country'] = $record['country'];
+            $customer['salesRepEmployeeNumber']= $record['salesRepEmployeeNumber'];
+            $customer['creditLimit']= $record['creditLimit'];
+
+            $output['customers'][] = $customer;
+        }
+
+        return $output;
+    }
+
+    public function findById(int $id): array
+    {
+        $pdo = $this->database->getConnection();
+        $stmt = $pdo->query("SELECT * FROM customers WHERE customerNumber = {$id}");
+        $record = $stmt->fetch();
+        if (gettype($record) !== 'array') {
+            throw new LogicException('data is not properly retrieved from DB');
+        }
+        $output['Customers data'] = $record;
+
+        return $output;
+    }
+
+    /**
+     * @throws PDOException
+     */
+    public function insert(array $userData): void
+    {
+        $pdo = $this->database->getConnection();
+        $sql = <<< INSERT_SQL
+            INSERT INTO
+              `customers` (
+                `customerNumber`,
+                `customerName`,
+                `contactLastName`,
+                `contactFirstName`,
+                `phone`,
+                `addressLine1`,
+                `addressLine2`,
+                `city`,
+                `state`,
+                `postalCode`,
+                `country`,
+                `salesRepEmployeeNumber`,
+                `creditLimit`
+              )
+            VALUES
+              (
+               {$userData['number']},
+                '{$userData['name']}',
+                '{$userData['lastName']}',
+                '{$userData['firstName']}',
+                '{$userData['phone']}',
+                '{$userData['addressLine1']}',
+                '{$userData['addressLine2']}',
+                '{$userData['city']}',
+                '{$userData['state']}',
+                {$userData['postalCode']},
+                '{$userData['country']}',
+                {$userData['salesEmployeeNumber']},
+                {$userData['creditLimit']}
+              );
+INSERT_SQL;
+        $pdo->exec($sql);
+    }
+    /**
+     * @throws PDOException
+     */
+    public function update(int $id, array $userData): void
+    {
+        $pdo = $this->database->getConnection();
+        $sql = "UPDATE `customers` SET";
+        if (!empty($userData['name'])) {
+            $sql .= "`customerName` = '{$userData['name']}',";
+        }
+        if (!empty($userData['lastName'])) {
+            $sql .= "`contactLastName` = '{$userData['lastName']}',";
+        }
+        if (!empty($userData['firstName'])) {
+            $sql .= "`contactFirstName` = '{$userData['firstName']}',";
+        }
+        if (!empty($userData['phone'])) {
+            $sql .= "`phone` = '{$userData['phone']}',";
+        }
+        if (!empty($userData['addressLine1'])) {
+            $sql .= "`addressLine1` = '{$userData['addressLine1']}',";
+        }
+        if (!empty($userData['addressLine2'])) {
+            $sql .= "`addressLine2` = '{$userData['addressLine2']}',";
+        }
+        if (!empty($userData['city'])) {
+            $sql .= "`city` = '{$userData['city']}',";
+        }
+        if (!empty($userData['state'])) {
+            $sql .= "`state` = '{$userData['state']}',";
+        }
+        if (!empty($userData['postalCode'])) {
+            $sql .= "`postalCode` = '{$userData['postalCode']}',";
+        }
+        if (!empty($userData['country'])) {
+            $sql .= "`country` = '{$userData['country']}',";
+        }
+        if (!empty($userData['salesEmployeeNumber'])) {
+            $sql .= "`salesRepEmployeeNumber` = '{$userData['salesEmployeeNumber']}',";
+        }
+        if (!empty($userData['creditLimit'])) {
+            $sql .= "`creditLimit` = '{$userData['creditLimit']}',";
+        }
+        $sql = substr($sql, 0, -1);
+        $sql .= "WHERE `customerNumber` = {$id}";
+        $sql .= ';';
+
+        $pdo->exec($sql);
+
+    }
+
+    /**
+     * @throws PDOException
+     */
+    public function delete(int $id): void
+    {
+        $pdo = $this->database->getConnection();
+        $pdo->exec("DELETE FROM customers WHERE customerNumber = {$id}");
+    }
+}
