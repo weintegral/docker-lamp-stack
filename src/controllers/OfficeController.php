48c756c14f3a766976c5bee39fb65e1de1e1d3da
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-27 01:01:17 -0500

    Updated OfficeController file

---
+++ src/controllers/OfficeController.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types = 1);
+namespace App\controllers;
+class OfficeController
+{
+    private Response $responseObj;
+    private OfficeModel $officeModel;
+
+    public function __construct()
+    {
+        $this->responseObj = ObjectContainer::response();
+        $this->officeModel = ObjectContainer::officeModel();
+    }
+
+    public function listAction(): string
+    {
+        try {
+            $output = $this->officeModel->findAll();
+            return $this->responseObj->toJson($output);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        } catch (InvalidArgumentException) {
+            return $this->responseObj->toJson(['status' => 'Invalid Argument Exception']);
+        }
+    }
+
+    public function getByIdAction(): string
+    {
+        try {
+            $urlPath= ObjectContainer::request()->getRequestPath();
+            $urlPathData = explode( '/', $urlPath);
+            $userProvidedOfficeId =(int)$urlPathData[2];
+
+            $output = $this->officeModel->findById($userProvidedOfficeId);
+            return $this->responseObj->toJson($output);
+        } catch (PDOException) {
+            return $this->responseObj->toJson(['status' => 'Database issue']);
+        } catch (LogicException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function createAction(): string
+    {
+        try {
+            $userProvidedData = ObjectContainer::request()->getRequestBody();
+            $this->officeModel->insert($userProvidedData);
+            return $this->responseObj->setResponseCode(201)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+    public function updateAction(): string
+    {
+        try {
+            $requestObj = ObjectContainer::request();
+
+            $urlPath = $requestObj->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedOfficeId = (int)$urlPathData[2];
+
+            $userProvidedData = $requestObj->getRequestBody();
+
+            $this->officeModel->update($userProvidedOfficeId, $userProvidedData);
+            return $this->responseObj->setResponseCode(200)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function deleteAction(): string
+    {
+        try {
+            $urlPath = ObjectContainer::request()->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedOfficeId = (int)$urlPathData[2];
+
+            $this->officeModel->delete($userProvidedOfficeId);
+            return $this->responseObj->setResponseCode(200)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+}
