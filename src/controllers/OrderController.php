a1deb5be7a09e34c8b53dc5dbe2a949a5b6363ac
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-02-27 01:01:41 -0500

    Updated OrderController file

---
+++ src/controllers/OrderController.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types = 1);
+namespace App\controllers;
+
+
+class OrderController
+{
+    private Response $responseObj;
+    private OrderModel $orderModel;
+
+    public function __construct()
+    {
+        $this->responseObj = ObjectContainer::response();
+        $this->orderModel = ObjectContainer::orderModel();
+    }
+
+    public function listAction(): string
+    {
+        try {
+            $output = $this->orderModel->findAll();
+            return $this->responseObj->toJson($output);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        } catch (InvalidArgumentException) {
+            return $this->responseObj->toJson(['status' => 'Invalid Argument Exception']);
+        }
+    }
+
+    public function getByIdAction(): string
+    {
+        try {
+            $urlPath = ObjectContainer::request()->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedOrderId = (int)$urlPathData[2];
+
+            $output = $this->orderModel->findById($userProvidedOrderId);
+            return $this->responseObj->toJson($output);
+        } catch (PDOException) {
+            return $this->responseObj->toJson(['status' => 'Database issue']);
+        } catch (LogicException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function createAction(): string
+    {
+        try {
+            $userProvidedData = ObjectContainer::request()->getRequestBody();
+            $this->orderModel->insert($userProvidedData);
+            return $this->responseObj->setResponseCode(201)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+    public function updateAction(): string
+    {
+        try {
+            $requestObj = ObjectContainer::request();
+
+            $urlPath = $requestObj->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidesdOrderId = (int)$urlPathData[2];
+
+            $userProvidedData = $requestObj->getRequestBody();
+
+            $this->orderModel->update($userProvidedOrderId, $userProvidedData);
+            return $this->responseObj->setResponseCode(200)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function deleteAction(): string
+    {
+        try {
+            $urlPath = ObjectContainer::request()->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedOrderId = (int)$urlPathData[2];
+
+            $this->orderModel->delete($userProvidedOrderId);
+            return $this->responseObj->setResponseCode(200)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+}
