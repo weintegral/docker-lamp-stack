236b5b02cbe373dccf87229eaf4596d611b4d441
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-01 23:49:43 -0500

    created Migration2 file

---
+++ src/migrations/Migration2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\migrations;
+
+class Migration2 extends BaseMigration
+{
+    public function run()
+    {
+        echo "Created Table called class". PHP_EOL;
+        $this->exec('');
+    }
+}
