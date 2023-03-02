5ebea6e5511805fb3820cd98d3b6357bf5da6b1a
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-02 00:17:55 -0500

    Updated Payment Controller

--- src/controllers/PaymentController.php
+++ src/controllers/PaymentController.php
@@ -1,7 +1,12 @@
 <?php
 declare(strict_types = 1);
 namespace App\controllers;
-
+use App\models\PaymentModel;
+use App\utils\ObjectContainer;
+use App\utils\Response;
+use InvalidArgumentException;
+use LogicException;
+use PDOException;
 class PaymentController
 {
     private Response $responseObj;
@@ -19,6 +24,8 @@ class PaymentController
             $output = $this->paymentModel->findAll();
             return $this->responseObj->toJson($output);
         } catch (PDOException $exception) {
+            logger($exception->getMessage());
+            logger($exception->getTraceAsString());
             return $this->responseObj->toJson(['status' => $exception->getMessage()]);
         } catch (InvalidArgumentException) {
             return $this->responseObj->toJson(['status' => 'Invalid Argument Exception']);
@@ -49,10 +56,30 @@ class PaymentController
             return $this->responseObj->setResponseCode(201)
                 ->toJson(['status' => 'success']);
         } catch (PDOException $exception) {
+            logger($exception->getMessage());
             return $this->responseObj->toJson(['status' => $exception->getMessage()]);
         }
     }
     public function updateAction(): string
+    {
+        try {
+            $requestObj = ObjectContainer::request();
+
+            $urlPath = $requestObj->getRequestPath();
+            $urlPathData = explode('/', $urlPath);
+            $userProvidedPaymentId = (int)$urlPathData[2];
+
+            $userProvidedData = $requestObj->getRequestBody();
+
+            $this->paymentModel->update($userProvidedPaymentId, $userProvidedData);
+            return $this->responseObj->setResponseCode(200)
+                ->toJson(['status' => 'success']);
+        } catch (PDOException $exception) {
+            return $this->responseObj->toJson(['status' => $exception->getMessage()]);
+        }
+    }
+
+    public function patchAction(): string
     {
         try {
             $requestObj = ObjectContainer::request();
