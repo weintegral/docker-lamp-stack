9526ae2b1a792fb78656869108e0ad0d6dbeb5f2
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-01 23:49:36 -0500

    created Migration1 file

---
+++ src/migrations/Migration1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\migrations;
+
+class Migration1 extends BaseMigration
+{
+    public function run()
+    {
+        echo "Created Database called school". PHP_EOL;
+        $this->exec('CREATE DATABASE IF NOT EXISTS `school`');
+    }
+}
