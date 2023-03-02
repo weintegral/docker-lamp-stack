d1f64e37ef6c4969fdcb7c2f075f1d95c0220499
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-02 00:15:15 -0500

    Updated Customer Controller

--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -26,6 +26,8 @@ class CustomerController
             $output = $this->customerModel->findAll();
             return $this->responseObj->toJson($output);
         } catch (PDOException $exception) {
+            logger($exception->getMessage());
+            logger($exception->getTraceAsString());
             return $this->responseObj->toJson(['status' => $exception->getMessage()]);
         } catch (InvalidArgumentException) {
             return $this->responseObj->toJson(['status' => 'Invalid Argument Exception']);
@@ -56,6 +58,7 @@ class CustomerController
             return $this->responseObj->setResponseCode(201)
                 ->toJson(['status' => 'success']);
         } catch (PDOException $exception) {
+            logger($exception->getMessage());
             return $this->responseObj->toJson(['status' => $exception->getMessage()]);
         }
     }
