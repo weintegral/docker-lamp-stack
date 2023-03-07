18c0fafe127877aad4598c6e84c3347e232e7316
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-07 13:36:39 -0500

    Update migrations file

--- public/migrations.php
+++ public/migrations.php
@@ -1,15 +1,10 @@
 <?php
 declare(strict_types = 1);
 
-ini_set('error_log', '/var/www/html/errors.log');
+use App\migrations\MainMigration;
 
-function dd($var, $message = ''): void
-{
-    var_dump($var);
-    die($message);
-}
-
-function logger(string $message)
-{
-    error_log($message, 3, '/var/www/html/errors.log');
+try {
+    (new MainMigration())->run();
+} catch(Throwable $exception) {
+    logger($exception->getMessage());
 }
