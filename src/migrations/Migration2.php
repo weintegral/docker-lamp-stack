19650e8a48150eeb776e8f4d85c98641c96a2bbb
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-07 13:38:00 -0500

    Updated Migration2 file

--- src/migrations/Migration2.php
+++ src/migrations/Migration2.php
@@ -1,12 +1,22 @@
 <?php
-
+declare(strict_types=1);
 namespace App\migrations;
 
 class Migration2 extends BaseMigration
 {
     public function run()
     {
-        echo "Created Table called class". PHP_EOL;
-        $this->exec('');
+        echo "Created Table called state" . PHP_EOL;
+        # heredoc syntax === ""
+        # nowdoc === ''
+        $sql = <<< SQL
+            CREATE TABLE IF NOT EXISTS state (
+                id int not null auto_increment,
+                name varchar(255) not null,
+                PRIMARY KEY (id)
+            );
+SQL;
+
+        $this->exec($sql);
     }
 }
