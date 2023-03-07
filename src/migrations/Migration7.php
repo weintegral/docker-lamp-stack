88c7be0eb268f6d57dbf9264be2a7954b52fac07
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-07 13:38:41 -0500

    Created Migration7 file

---
+++ src/migrations/Migration7.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+namespace App\migrations;
+
+class Migration7 extends BaseMigration
+{
+    public function run()
+    {
+        echo "Updated table address column city_id to not accept null values" . PHP_EOL;
+        $sql = <<< SQL
+            ALTER TABLE address
+            MODIFY city_id INT NOT NULL;
+            );
+SQL;
+
+        $this->exec($sql);
+    }
+}
