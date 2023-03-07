eee54368d47ad5c35da773df4c6ff5f4300cf364
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-03-07 13:37:29 -0500

    Updated Main migrations file

--- src/migrations/MainMigration.php
+++ src/migrations/MainMigration.php
@@ -8,5 +8,13 @@ class MainMigration
     {
         (new Migration1())->run();
         (new Migration2())->run();
+        (new Migration3())->run();
+        (new Migration4())->run();
+        (new Migration5())->run();
+        (new Migration6())->run();
+        (new Migration7())->run();
+        (new Migration8())->run();
+        (new Migration9())->run();
+        (new Migration10())->run();
     }
 }
